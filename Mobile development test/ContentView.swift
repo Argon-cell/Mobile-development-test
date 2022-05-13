44e452c639302b68c581ef9ff50344fcff1fea5d
Argon-cell <68294593+Argon-cell@users.noreply.github.com>
2022-05-13 05:56:33 +0300

    Initial Commit

---
+++ Mobile development test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mobile development test
+//
+//  Created by Ильгам Нафиков on 13.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mobile development test/Mobile_development_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_development_testApp.swift
+//  Mobile development test
+//
+//  Created by Ильгам Нафиков on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_development_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
